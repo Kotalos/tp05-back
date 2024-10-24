9d1db9730fa4c8e632833055b48c3323f7cd7177
Kotalos <kotalosyt@gmail.com>
2024-10-24 16:56:20 +0200

    change port

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/films', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
